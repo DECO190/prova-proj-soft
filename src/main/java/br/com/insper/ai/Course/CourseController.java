d2da2c40f7f81b5d988d55303db5c10f1b3eebb3
DECO190 <deco9871@gmail.com>
2024-09-23 11:07:50 -0300

    dev

--- src/main/java/br/com/insper/ai/Course/CourseController.java
+++ src/main/java/br/com/insper/ai/Course/CourseController.java
@@ -2,6 +2,7 @@ package br.com.insper.ai.Course;
 
 import br.com.insper.ai.Course.DTOs.UpdateCourseStudentsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,12 +14,13 @@ public class CourseController {
     private CourseService courseService;
 
     @PostMapping("/courses")
+    @ResponseStatus(HttpStatus.CREATED)
     public Course createCourse(@RequestBody  Course course) {
         return courseService.createCourse(course);
     }
 
     @GetMapping("/courses")
-    public List<Course> getCourses(@PathVariable(required = false) String courseName) {
+    public List<Course> getCourses(@RequestParam(required = false) String courseName) {
         return courseService.getCourses(courseName);
     }
 
